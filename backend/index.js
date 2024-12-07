b30a074ec494269c353c43ae80ae2ec56f6cf749
Sachin-Rawat07 <sachinrawatsr07@gmail.com>
2024-12-08 03:03:46 +0530

    user

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
 const express=require('express');
 const app=express();
+const bcrypt=require('bcrypt');
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
 
 require("dotenv").config();
 require("./conn/conn")
 
+// calling routes
+
+const User= require("./routes/user");
+
+//using routes
+app.use("/api/v1",User);
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello from backend Side")
+})
 app.listen(process.env.port  ,(req,res)=>{
     console.log(`server is running at port ${process.env.port}`);
     
